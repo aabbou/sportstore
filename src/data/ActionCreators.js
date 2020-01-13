2e172939e30b6a20d3b2c0ce9be4be52700630af
Seekar <ayoub.ab03@mail.com>
2020-01-13 21:16:32 +0100

    Creating the data store

---
+++ src/data/ActionCreators.js
@@ -0,0 +1,10 @@
+import {ActionsTypes} from "./Types";
+import {data as phData} from "./placeholderData";
+
+export const loadData = (dataType) => ({
+    type : ActionsTypes.DATA_LOAD,
+    payload : {
+        dataType : dataType,
+        data : phData[dataType]
+    }
+});
